68b7d88eb82977d19348d4b86365817323b06780
fdc.johntiu <fdc.johntiu@gmail.com>
2023-06-08 10:35:33 +0800

    payment details changed

--- app/src/main/java/com/example/techhaven/ui/comment/CommentFragment.java
+++ app/src/main/java/com/example/techhaven/ui/comment/CommentFragment.java
@@ -4,8 +4,13 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,6 +18,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -24,6 +30,7 @@ import com.example.techhaven.CommentAdapter;
 import com.example.techhaven.R;
 import com.example.techhaven.VolleySingleton;
 import com.example.techhaven.databinding.FragmentCommentBinding;
+import com.example.techhaven.ui.webview.WebViewFragment;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -38,6 +45,40 @@ public class CommentFragment extends Fragment {
     private List<Comment> commentList;
     private CommentAdapter adapter;
     private RecyclerView recyclerView;
+    private Button shopeeBtn;
+    private Button lazadaBtn;
+    private WebView webView;
+
+    private void loadWebsite(String url) {
+        webView.loadUrl(url);
+    }
+
+    private void setupButtons() {
+        Button shopeeBtn = getView().findViewById(R.id.shopee_btn);
+        Button lazadaBtn = getView().findViewById(R.id.lazada_btn);
+
+        shopeeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadWebsite("www.shopee.com");
+            }
+        });
+
+        lazadaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadWebsite("www.lazada.com");
+            }
+        });
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Call the setupButtons method
+        setupButtons();
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -49,14 +90,19 @@ public class CommentFragment extends Fragment {
 
         recyclerView = root.findViewById(R.id.recyclerView);
         commentList = new ArrayList<>();
+        shopeeBtn = root.findViewById(R.id.shopee_btn);
+        lazadaBtn = root.findViewById(R.id.lazada_btn);
+        webView = root.findViewById(R.id.webview_container);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        webView.setWebViewClient(new WebViewClient());
 
+        RecyclerView recyclerView = root.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         getComments();
 
-
         return root;
-
     }
 
     public void getComments() {
@@ -73,7 +119,6 @@ public class CommentFragment extends Fragment {
                         for (int i = 0; i < jsonArray.length(); i++) {
 
                             JSONObject object = jsonArray.getJSONObject(i);
-
                             Comment comment = new Comment();
                             comment.setBody(object.getString("body"));
 
@@ -113,5 +158,15 @@ public class CommentFragment extends Fragment {
         binding = null;
     }
 
-
+    // Add the onOptionsItemSelected method here
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            // Handle the back button click
+            requireActivity().onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
